c5f7c102cf6878806d0899a56a6835712987e9b8
rx1996 <3343108599@qq.com>
2017-06-20 18:32:16 +0800

    一  :  欢迎界面及主界面布局

---
+++ app/src/main/java/com/atguigu/p2pmodule/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.p2pmodule.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.atguigu.p2pmodule.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
