f1aec9c0341a466839f313063dcc226a7d526f90
rx1996 <3343108599@qq.com>
2017-06-24 10:58:14 +0800

    十五 :   自定义属性

--- app/src/main/java/com/atguigu/p2pmodule/view/ProgressView.java
+++ app/src/main/java/com/atguigu/p2pmodule/view/ProgressView.java
@@ -2,6 +2,7 @@ package com.atguigu.p2pmodule.view;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,6 +11,7 @@ import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.atguigu.p2pmodule.R;
 import com.atguigu.p2pmodule.utils.UIUtils;
 
 
@@ -31,19 +33,33 @@ import com.atguigu.p2pmodule.utils.UIUtils;
 * */
 public class ProgressView extends View {
     private Paint paint;
-    private int paintColor = Color.BLACK;
+    private int paintColor;
     private int steokeWidth = UIUtils.dp2px(20);
     private int heigth;
     private int width;
     private int sweepAngle = 0;
+    private int textColor;
     public ProgressView(Context context) {
         super(context);
         init();
     }
-
+    /*
+        * 自定义属性 三步
+        * 第一步 创建attrs文件
+        * 第二步 在自定义控件构造器方法中 实例化attrs对象并获取属性名称和默认值
+        * 第三步 在布局文件中使用自定义的属性
+        * */
     public ProgressView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.progressView);
+        int color = typedArray.getColor(R.styleable.progressView_paintColor,Color.BLACK);
+        this.paintColor = color;
+        int tc = typedArray.getColor(R.styleable.progressView_textColor,Color.BLUE);
+        textColor = tc;
+        //释放资源
+        typedArray.recycle();
+
     }
     private void init(){
         paint = new Paint();
@@ -74,9 +90,9 @@ public class ProgressView extends View {
         * */
         //画圆
         paint.setStrokeWidth(steokeWidth);//画笔的宽度
-        paint.setColor(Color.BLACK);//画笔的颜色
-        int cx = width / 2;//画笔的x坐标
-        int cy = heigth / 2;//画笔的Y坐标
+        paint.setColor(paintColor);//圆环的颜色
+        int cx = width / 2;//圆心的x坐标
+        int cy = heigth / 2;//圆心的Y坐标
         int radius = cx - steokeWidth / 2;//圆环的半径
         canvas.drawCircle(cx,cy,radius,paint);
 
@@ -88,6 +104,7 @@ public class ProgressView extends View {
         canvas.drawArc(rectF,0,sweepAngle,false,paint);
 
         //画文字
+        paint.setColor(textColor);
         paint.setStrokeWidth(0);//设置画笔的宽度
         String str = sweepAngle+"%";
         paint.setTextSize(30);//设置文子大小
@@ -104,6 +121,16 @@ public class ProgressView extends View {
     * invalidate和postInvalidate的区别是什么
     * invalidate是主线程进行强制重绘
     * postInvalidate是分线程进行强制重绘
+    * */
+    /*
+    * 面试题：
+    * android中动画有几种？
+    * 三种
+    * 1 属性动画 ：真正的改变了控件的属性
+    * 2 帧动画 ： 把多张图片串联起来实现连续播放的效果
+    * 3 视图动画 ：普通动画虽然控件位置或者大小发生了变化，但属性并没有真正的改变，控件的监听位置并无发生改变
+    *
+    *
     * */
     public void setSweepAngle(final int sweepAngle){
 //        for (int i = 0; i < sweepAngle; i++) {
